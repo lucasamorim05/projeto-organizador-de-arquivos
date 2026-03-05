5b7046c2fb0296015ecc5f8cdcb6d35cfc2bb3fb
Lucas de Oliveira Amorim <lcoliveiraamorim50@yahoo.com>
2026-03-05 19:53:04 -0300

    versão 0.1: implementando função listar_arquivos

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import os
 
+def listar_arquivos(pasta):
+    try:
+        arquivos = os.listdir(pasta) #acessa a pasta e pega seus arquivos
+        print("\narquivos encontrados!\n")
+
+        for arquivo in arquivos: #para cada arquivo encontrado
+            caminho_completo = os.path.join(pasta, arquivo) #junta pasta e arquivo e cria o caminho completo
+
+            if os.path.isfile(caminho_completo):
+                print(arquivo) #testa o caminho, e se funcionar, ele imprime
+    
+    except FileNotFoundError: #se a pasta não foi encontrada
+        print("A pasta não foi encontrada.")
+
+    except PermissionError: #se não conseguiu acessa-la
+        print("Sem permissão para acessar essa pasta.")
+
 def main():
     print("== Organizador de arquivos v0.1 ==")
     pasta = input("Digite o caminho da pasta que deseja organizar: ")
+    listar_arquivos(pasta)
 
 if __name__ == "__main__":
     main()
