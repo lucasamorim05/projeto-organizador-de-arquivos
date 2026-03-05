3e5d8dfa7e7861d9d9864d9283ffef3609b8ae52
Lucas de Oliveira Amorim <lcoliveiraamorim50@yahoo.com>
2026-03-05 19:44:00 -0300

    versão 0.1: Importando os (bliblioteca para explorar aqruivos) e criando main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+import os
+
+def main():
+    print("== Organizador de arquivos v0.1 ==")
+    pasta = input("Digite o caminho da pasta que deseja organizar: ")
+
+if __name__ == "__main__":
+    main()
