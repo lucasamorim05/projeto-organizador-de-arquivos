dd8a608144abbdf8be57981484662d814ca0088e
lucas-chromebook <lucasamorim50@yahoo.com>
2026-03-17 20:44:20 -0300

    versão 0.5: inserindo registrar_log na função organizar_arquivos

--- main.py
+++ main.py
@@ -52,12 +52,14 @@ def organizar_arquivos(pasta,acoes):
         shutil.move(origem,destino)
         print(f"{arquivo} movido para {categoria}")
 
+        registrar_log(arquivo, categoria)
+
 def registrar_log(arquivo,categoria):
     with open("log_organizador.txt","a",encoding="utf-8") as log:
         log.write(f"{arquivo} -> {categoria}\n")
 
 
-    
+
 def main():
     print("== Organizador de arquivos v0.5 ==")
     pasta = input("Digite o caminho da pasta que deseja organizar: ")
