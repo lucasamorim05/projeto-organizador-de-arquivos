7a0b715a30c17848b0030e4d2c04f5795d69faa6
Lucas de Oliveira Amorim <lcoliveiraamorim50@yahoo.com>
2026-03-21 20:04:20 -0300

    versão 0.7: criando janela e título

--- interface.py
+++ interface.py
@@ -1,3 +1,18 @@
 import tkinter as tk #biblioteca para criar janelas (interfaces gráficas)
 from tkinter import filedialog #abre janela para escolher pasta
 from main import executar_organizacao #função onde está a lógica do organizador
+
+def selecionar_pasta():
+def organizar():
+
+#Cria janela
+janela = tk.Tk()
+janela.title("Organizador de Arquivos")
+janela.geometry("500x250")
+
+#Cria título
+titulo = tk.Label(janela, text="Organizador de Arquivos", font=("Arial", 16))
+titulo.pack(pady=10)
+
+
+
