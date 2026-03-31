dfaa27db1b0932c2082ebef379b81aa3b5b68756
Lucas de Oliveira Amorim <lcoliveiraamorim50@yahoo.com>
2026-03-31 20:25:02 -0300

    versão 0.8: criando classe MonitorHandler

--- monitor.py
+++ monitor.py
@@ -5,3 +5,15 @@ from watchdog.events import FileSystemEventHandler      #reage a eventos
 import time                                             #controla pausas (evita travar CPU)
 import os                                               
 from main import executar_organizacao                   #função do main.py
+
+
+class MonitorHandler(FileSystemEventHandler):           #classe que reage a eventos do sistema
+
+    def __init__(self, pasta):                          #recebe a pasta que será monitorada
+        self.pasta = pasta                              #guarda essa informação dentro da classe
+
+    def on_created(self, event):
+        if not event.is_directory:                      #verifica se é arquivo, e não pasta
+            print(f"Novo arquivo detectado: {event.src_path}")
+            executar_organizacao(self.pasta)
+
